9b92ec222367652d0e9ef4ea9733b7cba1414968
Giuseppe Varriale <pepe.saoleo@gmail.com>
2020-05-27 23:59:49 +0000

    Refact UserController.selectAll() to work with HttpRequest class

--- routes/users.js
+++ routes/users.js
@@ -14,7 +14,7 @@ router.get("/", function (req, res, next) {
   client.get("/users", function (err, request, response, obj) {
     assert.ifError(err);
 
-   res.end(JSON.stringify(obj, null, 2));
+    res.end(JSON.stringify(obj, null, 2));
   });
 });
 
